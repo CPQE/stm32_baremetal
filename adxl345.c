49f2b6d8707ea0d4d71f1709780b888a4b6272fe
Cyrus Parvereshi <cyrus.parvereshi@ucdavis.edu>
2026-04-09 18:48:48 -0700

    did spi lab  need to test though

---
+++ adxl345.c
@@ -0,0 +1,33 @@
+#include "adx1345.h"
+
+void adxl_read(uint8_t address, uint8_t *rxdata){
+	//set read op
+	address |= ADXL345_READ_OPERATION;
+	//enable multi-byte 
+	address |= ADXL345_MULTI_BYTE_ENABLE; //enable multi-byte
+	cs_enable(); //pull cs line low to enable slave
+	spi1_transmit(&address,1); //send address
+	spi1_receive(rxdata,6); //read 6 bytes
+	cs_disable(); //pull cs line high to disable slave
+}
+
+void adx1_write(uint8_t address, uint8_t value){
+	uint8_t data[2]; 
+	//enable multi-byte place address into buffer
+	data[0] = address | ADXL345_MULTI_BYTE_ENABLE;
+	//put data into buffer
+	data[1] = value; 
+	cs_enable(); 
+	spi1_transmit(data, 2); 
+	cs_diable(); 
+}
+
+void adx1_init(void){
+	//enable spi GPIO
+	spi_gpio_init(); 
+	spi1_config(); 
+	adxl_write(ADXL345_REG_DATA_FORMAT, ADXL345_RANGE_4G); 
+	adxl_write(ADXL345_REG_POWER_CTL, ADXL345_RESET); 
+	adxl_write(ADXL345_REG_POWER_CTL, ADXL345_MEASURE_BIT); 
+}
+
